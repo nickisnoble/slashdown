d15c698425765a6e7707d01ab80b98836eb01731
Nick Noble <nick.noble@hey.com>
2021-02-18 21:02:49 -0500

    update file structure

---
+++ index.js
@@ -0,0 +1,7 @@
+import Lexer from './src/Lexer'
+import Parser from './src/Parser'
+
+export default {
+  Lexer,
+  Parser
+}
--- src/index.js
+++ src/index.js
@@ -1,55 +1,50 @@
-import Lexer from './classes/Lexer'
-import Parser from './classes/Parser'
+import Lexer from './Lexer'
+import Parser from './Parser'
 
-export default {
-  Lexer,
-  Parser
-}
-
-// // DEBUG
-// const source = `
-//   /hero is-sticky
+// DEBUG
+const source = `
+  /hero is-sticky
     
-//     # This is proseCMS
-//     It's a dead simple content management system / markdown flavor.
+    # This is proseCMS
+    It's a dead simple content management system / markdown flavor.
 
-//   // This is a comment
-//   /columns 3 equal
+  // This is a comment
+  /columns 3 equal
 
-//     /column A
+    /column A
 
-//       ### Dead simple
-//       - Portable Markdown + layout
-//       - Realtime preview
-//       - drop in, batteries not needed
-
-
-//     /column B
-
-//       ### Works how you want
-//       - As a headless CMS
-//       - With a database
+      ### Dead simple
+      - Portable Markdown + layout
+      - Realtime preview
+      - drop in, batteries not needed
 
 
-//     /column C
+    /column B
 
-//       ### Hackable AF
-//       - Add your own functions
-//       - Style the editor
+      ### Works how you want
+      - As a headless CMS
+      - With a database
 
-//   /footer
-// `
 
-// const lex = new Lexer( source );
-// const parser = new Parser( lex.tokens );
-// const pretty = JSON.stringify( parser.ast, null, 2 )
-// // const types = tokens
-//   // .map( t => `[${t.type}] ${t.text == "\n" ? "\\n" : t.text }` )
+    /column C
 
-// document.body.innerHTML = `
-//   <pre>
-//     <code>
-// ${pretty}
-//     </code>
-//   </pre>
-// `
+      ### Hackable AF
+      - Add your own functions
+      - Style the editor
+
+  /footer
+`
+
+const lex = new Lexer( source );
+const parser = new Parser( lex.tokens );
+const pretty = JSON.stringify( parser.ast, null, 2 )
+// const types = tokens
+  // .map( t => `[${t.type}] ${t.text == "\n" ? "\\n" : t.text }` )
+
+document.body.innerHTML = `
+  <pre>
+    <code>
+${pretty}
+    </code>
+  </pre>
+`
