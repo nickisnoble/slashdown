8197eebc7b767c61fd5f9471b592e835e44701dc
Nick Noble <nick.noble@hey.com>
2021-02-19 23:13:41 -0500

    update exported function

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import Lexer from './src/Lexer'
 import Parser from './src/Parser'
+import Renderer from './src/Renderer'
 
 export default {
   Lexer,
-  Parser
+  Parser,
+  Renderer
+}
+
+export function slashdown( raw ) {
+  const lexed = new Lexer( raw );
+  const parsed = new Parser( lexed.tokens );
+  const rendered = new Renderer( parsed.ast );
+
+  return rendered.markup;
 }
