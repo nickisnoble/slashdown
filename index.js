cb96f7ece67126dffc4b33c03da1e49eb4dcf2cd
Nick Noble <nick.noble@hey.com>
2021-02-19 22:16:15 -0500

    add processing function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import Lexer from './src/Lexer'
 import Parser from './src/Parser'
 
+export function process( source ) {
+  const lex = new Lexer( source );
+  const parser = new Parser( lex.tokens );
+    
+  return parser.ast;
+}
+
 export default {
   Lexer,
   Parser
