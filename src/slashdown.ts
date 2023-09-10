7ddb36552e7df7179fc805a41384a4d9042c4daa
Nick Noble <nick@miniware.team>
2023-09-10 15:29:13 -0400

    add slashdown with basic rendering

--- src/slashdown.ts
+++ src/slashdown.ts
@@ -1,8 +1,37 @@
 import type { SD } from "./types";
-import renderer from "./renderers/json"; // TODO: Switch default to HTML
+import JSONRenderer from "./renderers/json"; // TODO: Switch default to HTML
+import { Lexer } from "./lexer";
+import { Parser } from "./parser";
 
-export class Slashdown {
-  constructor( src: string = "", renderer: SD.Renderer = renderer ) {
+class Slashdown {
+  src: string;
+  render: SD.Renderer;
 
+  constructor( src = "", renderer = JSONRenderer ) {
+    this.src = src;
+    this.render = renderer;
   }
+
+  process( src = this.src ) {
+    this.src = src;
+
+    const tokens = new Lexer( src ).tokens();
+    const ast = new Parser( tokens ).ast();
+    return this.render( ast );
+  }
+}
+
+
+function sd(strings: TemplateStringsArray, ...values: any[]) {
+  const fullString = strings.reduce((result, string, i) => (
+    result + string + (values[i] || '')
+  ), '').replace(/^\n+|\n+$/g, ''); // remove leading or trailing newlines
+
+  return new Slashdown(fullString).process();
+}
+
+
+export {
+  Slashdown,
+  sd
 }
---
+++ test/slashdown.test.ts
@@ -0,0 +1,54 @@
+import type { SD } from '../src/types'
+import { expect, test, describe, vi } from 'vitest'
+import { Slashdown, sd } from "../src/slashdown";
+import { dedent } from './utils';
+
+test( "it works", ()=> {
+  const src = dedent`
+    //  Welcome to slashdown!
+    //  A superset of markdown for quickly creating web pages.
+    //  Plays *very* well with: HTMX, Tailwind, AlpineJS, and the backend of your choice.
+
+    // This is a comment. It will not be rendered in the output.
+
+    /main .container.mx-auto
+      // Any line starting with a / is a tag, unless it's a comment.
+      // You can add classes and ids to tags via the usual shorthands (e.g. .class #id)
+      // You can also add attributes the normal way! (e.g. href="https://example.com" )
+
+      // Anything not starting with a / is a child of the previous tag, and will be rendered as markdown
+      // Indentation is important! It determines the parent/child relationship of tags.
+
+      # This is a heading. It will be rendered as an h1 tag.
+
+      It's the child of the section element above it.
+
+      / .grid.grid-col-2
+        data-foo="bar"
+
+        This will render as a paragraph.
+
+        And this will render as a paragraph too.
+
+        // Boolean attributes are supported too!
+        /input type="text" placeholder="Enter your name" autofocus
+
+        // An empty tag will be rendered as a <div> tag.
+        // And items on the immediate next line after a tag, if indented, will be parsed as attributes.
+
+      // Items at the same indent level become siblings.
+      /footer .flex.justify-between
+        Made with ❤️ by slashdown.
+
+        // Plaintext can be easily added with " = " at the end of a tag
+        /a href="https://miniware.team" target="_blank" = Made by Miniware
+  `
+  const slashdown = new Slashdown()
+  expect( slashdown.process(src) ).toBeTypeOf("string");
+})
+
+test( "shorthand works", () => {
+  expect( sd`
+    / .container = Hello World!
+  ` ).toBeTypeOf("string")
+})
