2fa5e96be4e495d5c1420c916c862edee8522861
Nick Noble <nick@miniware.team>
2023-09-30 23:39:19 -0400

    fix attributes on new lines

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,17 +1,17 @@
 import type { SD } from "./types"
 
 const patterns = {
-  "Tag":       /^\/([\w-]*)/,
-  "Id":        /^#([\w-]+)/,
-  "Class":     /^\.([\w-]+)/,
+  "Tag": /^\/([\w-]*)/,
+  "Id": /^#([\w-]+)/,
+  "Class": /^\.([\w-]+)/,
   "Attribute": /^([\w-]+="[^"]*")|^([\w-]+)/,
-  "Text":      /^=\s+(.+)$/
+  "Text": /^=\s+(.+)$/
 };
 
 const isComment = (line: string): boolean => !!line.match(/^\s*\/\//);
 const isBlank = (line: string): boolean => line.trim() === "";
 
-const spacesPreceding = ( line: string ): number => {
+const spacesPreceding = (line: string): number => {
   const match = line.match(/^\s*/);
   return match ? match[0].length : 0;
 }
@@ -33,7 +33,30 @@ export class Lexer {
     const lookahead = (n: number = 1): string => lines[i + n];
     const isTagStart = (l: string): boolean => l.trim().startsWith('/');
 
-    function lexTagLines( startingLine: string, tagIndentLevel: number ): void {
+
+    primary: while (i < lines.length) {
+      const line = lines[i];
+
+      // Skip blanks at top level.
+      // (They are only important inside Markdown!)
+      if (isComment(line) || isBlank(line)) {
+        i++;
+        continue primary;
+      }
+
+      const indentation = spacesPreceding(line);
+
+      if (isTagStart(line)) {
+        lexTagLines(line, indentation);
+        i++;
+
+      } else {
+        lexMarkdownLines(line, indentation);
+        i++;
+      }
+    }
+
+    function lexTagLines(startingLine: string, tagIndentLevel: number): void {
       let remainingText = startingLine.trim();
 
       // Lex first line
@@ -41,51 +64,59 @@ export class Lexer {
       do {
         matchFound = false;
 
-        for (const type of ["Tag", "Class", "Id", "Attribute", "Text"] as const) {
+        typeLoop: for (const type of ["Tag", "Class", "Id", "Attribute", "Text"] as const) {
           const match = remainingText.match(patterns[type]);
 
           if (match) {
             tokenList.push({
               type,
-              content: match[1] ?? match[2], // Grab the capture group content. Some have multiple possibilities!
+              content: match[1] ?? match[2] ?? match[0], // Grab the capture group content. Some have multiple possibilities!
               indent: tagIndentLevel
             });
 
             remainingText = remainingText.slice(match[0].length).trim();
             matchFound = true;
-            break; // break the for loop and start over because we found a match
+            break typeLoop; // break the for loop and start over because we found a match
           }
         }
       } while (matchFound);
 
-      // Lex subsequent lines (if any)
-      const moreTagDetailsMayExist = () => {
-        const nextLine = lookahead(1)
-        return !!nextLine && !isBlank( nextLine ) && !isTagStart( nextLine )
-      }
-      while ( moreTagDetailsMayExist() ) {
-        // Check if the whole line matches something valid
-        const nextLine = lookahead(1).trim()
+      let nextLine = lookahead(1)
+      while (!!nextLine && !isBlank(nextLine) && !isTagStart(nextLine)) {
+        nextLine = nextLine.trim()
+        let matchFound = false;
 
-        for (const type of ["Class", "Id", "Attribute", "Text"] as const) {
-          const match = nextLine.match(patterns[type]);
+        typeLoop: for (const type of ["Class", "Id", "Attribute", "Text"] as const) {
+          const match = nextLine.match(
+            new RegExp(
+              (patterns[type].source + "$") // check ENTIRE line
+              .replace("$$", "$") // (some regex already check for line-end)
+            )
+          );
 
           if (match) {
             tokenList.push({
               type,
-              content: match[1] ?? match[2], // Use match[1] to exclude the prefix (/, ., #, =)
+              content: match[1] ?? match[2] ?? match[0],
               indent: tagIndentLevel
-            });
+            })
 
-            i++; // count the line since it was valid
+            nextLine = nextLine.slice(match[0].length).trim(); // remove the matched part from nextLine
+            matchFound = true;
+            break typeLoop;
           }
         }
 
-        break; // Break out if there's no match, passing any remaining content back to the main loop.
+        if (!matchFound) {
+          break; // if no match was found, break the while loop
+        }
+
+        i++; // count the line since it was valid
+        nextLine = lookahead(1); // update nextLine for the next iteration
       }
     }
 
-    function lexMarkdownLines( startingLine: string, startingIndentLevel: number ): void {
+    function lexMarkdownLines(startingLine: string, startingIndentLevel: number): void {
       const markdownToken: { type: "Markdown" } & SD.Token = {
         type: "Markdown",
         content: startingLine.trim(),
@@ -96,7 +127,7 @@ export class Lexer {
         const nextLine = lookahead(1)
         if (nextLine === undefined) return false;
 
-        const notATag = !isTagStart( nextLine );
+        const notATag = !isTagStart(nextLine);
         const sameBlock = spacesPreceding(nextLine) >= startingIndentLevel;
 
         // If the next line:
@@ -107,49 +138,25 @@ export class Lexer {
         return sameBlock && notATag || isBlank(nextLine);
       }
 
-      while( markdownRemains() ) {
+      while (markdownRemains()) {
         i++; // count the line
         const line = lines[i];
 
-        if( isBlank( line ) ) {
+        if (isBlank(line)) {
           markdownToken.content += "\n";
           continue;
         }
 
-         const dedentedLine = line.slice(spacesPreceding(line));
-         markdownToken.content += "\n" + dedentedLine;
+        const dedentedLine = line.slice(spacesPreceding(line));
+        markdownToken.content += "\n" + dedentedLine;
       }
 
 
       // Remove any trailing newline.
       markdownToken.content = markdownToken.content.trim()
 
-      tokenList.push( markdownToken )
+      tokenList.push(markdownToken)
     }
-
-    while (i < lines.length) {
-      const line = lines[i];
-
-      // Skip blanks at top level.
-      // (They are only important inside Markdown!)
-      if (isComment(line) || isBlank(line)) {
-        i++;
-        continue;
-      }
-
-      const indentation = spacesPreceding(line);
-
-      // If the line starts with a tag
-      if (isTagStart( line )) {
-        lexTagLines(line, indentation);
-        i++;
-      } else {
-        // If the line is markdown
-        lexMarkdownLines(line, indentation);
-        i++;
-      }
-    }
-
     return tokenList;
   }
 }
--- test/lexer.test.ts
+++ test/lexer.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest'
+import { expect, test, describe } from 'vitest'
 import type { SD } from "../src/types";
 import { Lexer } from '../src/lexer'
 import { dedent } from "./utils"
@@ -28,7 +28,7 @@ test('can lex a simple tag', () => {
     }
   ]
 
-  expect(tokens).toEqual( expected )
+  expect(tokens).toEqual(expected)
 })
 
 test('can lex a blank tag with a class', () => {
@@ -52,54 +52,104 @@ test('can lex multiple chained selectors', () => {
   ])
 })
 
-test('attributes work', () => {
-  const src = '/div data-foo="bar baz"'
-  const lexer = new Lexer(src)
-  const tokens = lexer.tokens()
-  expect(tokens).toEqual([
-    { type: 'Tag', content: 'div', indent: 0 },
-    { type: 'Attribute', content: 'data-foo="bar baz"', indent: 0 }
-  ])
-})
+describe("attributes", () => {
+  test('attributes work', () => {
+    const src = '/div data-foo="bar baz"'
+    const lexer = new Lexer(src)
+    const tokens = lexer.tokens()
+    expect(tokens).toEqual([
+      { type: 'Tag', content: 'div', indent: 0 },
+      { type: 'Attribute', content: 'data-foo="bar baz"', indent: 0 }
+    ])
+  })
 
-test('can have both classes and attributes', () => {
-  const src = '/div .my-class data-foo="bar baz"'
-  const lexer = new Lexer(src)
-  const tokens = lexer.tokens()
-  expect(tokens).toEqual([
-    { type: 'Tag', content: 'div', indent: 0 },
-    { type: 'Class', content: 'my-class', indent: 0 },
-    { type: 'Attribute', content: 'data-foo="bar baz"', indent: 0 }
-  ])
-})
+  test('can have all syntaxes on the same line', () => {
+    const src = '/div .my-class#id data-foo="bar baz" autofocus'
+    const lexer = new Lexer(src)
+    const tokens = lexer.tokens()
+    expect(tokens).toEqual([
+      { type: 'Tag', content: 'div', indent: 0 },
+      { type: 'Class', content: 'my-class', indent: 0 },
+      { type: 'Id', content: 'id', indent: 0 },
+      { type: 'Attribute', content: 'data-foo="bar baz"', indent: 0 },
+      { type: 'Attribute', content: 'autofocus', indent: 0 }
+    ])
+  })
 
-test('can lex attributes on a new line', () => {
-const src = dedent`
-  /div
-    data-foo="bar baz"
-    This is content
-`
-  const lexer = new Lexer(src)
-  const tokens = lexer.tokens()
-  expect(tokens).toEqual([
-    { type: 'Tag', content: 'div', indent: 0 },
-    { type: 'Attribute', content: 'data-foo="bar baz"', indent: 0 },
-    { type: 'Markdown', content: 'This is content', indent: 2 }
-  ])
+  test('on a new line', () => {
+    const src = dedent`
+      /div
+        data-foo="bar baz"
+        This is content
+    `
+    const lexer = new Lexer(src)
+    const tokens = lexer.tokens()
+    expect(tokens).toEqual([
+      { type: 'Tag', content: 'div', indent: 0 },
+      { type: 'Attribute', content: 'data-foo="bar baz"', indent: 0 },
+      { type: 'Markdown', content: 'This is content', indent: 2 }
+    ])
+  })
+
+  test('boolean on a new line', () => {
+    const src = dedent`
+      /div
+        autofocus
+        This is content
+    `
+    const lexer = new Lexer(src)
+    const tokens = lexer.tokens()
+    expect(tokens).toStrictEqual([
+      { type: 'Tag', content: 'div', indent: 0 },
+      { type: 'Attribute', content: 'autofocus', indent: 0 },
+      { type: 'Markdown', content: 'This is content', indent: 2 }
+    ])
+  })
+
+  test('multiple on new lines', () => {
+    const src = dedent`
+      /input
+        type="text"
+        autofocus
+        data-foo="bar baz"
+        This is content
+    `
+    const lexer = new Lexer(src)
+    const tokens = lexer.tokens()
+    expect(tokens).toEqual([
+      { type: 'Tag', content: 'input', indent: 0 },
+      { type: 'Attribute', content: 'type="text"', indent: 0 },
+      { type: 'Attribute', content: 'autofocus', indent: 0 },
+      { type: 'Attribute', content: 'data-foo="bar baz"', indent: 0 },
+      { type: 'Markdown', content: 'This is content', indent: 2 }
+    ])
+  })
+
+  test('with text content', () => {
+    const src = `/div data-foo="bar baz" autofocus = This is text`
+    const lexer = new Lexer(src)
+    const tokens = lexer.tokens()
+    expect(tokens).toEqual([
+      { type: 'Tag', content: 'div', indent: 0 },
+      { type: 'Attribute', content: 'data-foo="bar baz"', indent: 0 },
+      { type: 'Attribute', content: 'autofocus', indent: 0 },
+      { type: 'Text', content: 'This is text', indent: 0 }
+    ])
+  })
 })
 
 test('lexes indentation and nested items properly', () => {
   const src = dedent`
-    /ul .list
-      /li
-    // This is a comment which should be ignored
-      /li
-        data-foo="bar baz"
-        /span
-    /footer
+  /ul .list
+    /li
+  // This is a comment which should be ignored
+    /li
+      data-foo="bar baz"
+      /span
+  /footer
 
-      This is outdented content
-  `
+    This is outdented content
+`
 
   const lexer = new Lexer(src)
   const tokens = lexer.tokens()
@@ -115,30 +165,17 @@ test('lexes indentation and nested items properly', () => {
   ])
 })
 
-test('can lex a tag with attributes and text content', () => {
-  const src = `/div data-foo="bar baz" autofocus = This is text`
-  const lexer = new Lexer(src)
-  const tokens = lexer.tokens()
-  expect(tokens).toEqual([
-    { type: 'Tag', content: 'div', indent: 0 },
-    { type: 'Attribute', content: 'data-foo="bar baz"', indent: 0 },
-    { type: 'Attribute', content: 'autofocus', indent: 0 },
-    { type: 'Text', content: 'This is text', indent: 0 }
-  ])
-})
-
-
 test('can lex markdown only', () => {
   const src = dedent`
-    # This is a markdown block
+  # This is a markdown block
 
-    Here is some text.
-    - and
-    - a
-    - list
+  Here is some text.
+  - and
+  - a
+  - list
 
-    More text here.
-  `;
+  More text here.
+`;
   const lexer = new Lexer(src)
   const tokens = lexer.tokens()
 
@@ -147,19 +184,18 @@ test('can lex markdown only', () => {
   ])
 })
 
-
 test('can lex long markdown inside tag', () => {
   const src = dedent`
-    / .container
-      # This is a markdown block
+  / .container
+    # This is a markdown block
 
-      Here is some text.
-      - and
-      - a
-      - list
+    Here is some text.
+    - and
+    - a
+    - list
 
-      More text here.
-  `;
+    More text here.
+`;
   const lexer = new Lexer(src)
   const tokens = lexer.tokens()
 
@@ -169,15 +205,15 @@ test('can lex long markdown inside tag', () => {
     {
       type: 'Markdown',
       content: dedent`
-        # This is a markdown block
+      # This is a markdown block
 
-        Here is some text.
-        - and
-        - a
-        - list
+      Here is some text.
+      - and
+      - a
+      - list
 
-        More text here.
-      `,
+      More text here.
+    `,
       indent: 2
     },
   ])
